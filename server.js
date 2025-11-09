617afcebe077c31aca4c17d31d1b44d451143c4c
chanin <karaoke@24335184.cloudvps.in.th>
2025-11-10 05:04:05 +0700

    chore: wipe old project

--- server.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-// ตอนจะใช้ ก็เรียกจาก process.env
-const apiKey = process.env.PAYMENT_API_KEY;
-const dbUrl = process.env.DATABASE_URL;
-const port = process.env.PORT;
